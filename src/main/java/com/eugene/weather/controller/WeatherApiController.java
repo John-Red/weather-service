f73b9ccf998dfeb757e383325131519df1983c14
Evg <redin.job@gmail.com>
2022-10-15 13:05:48 +0300

    add service stub

--- src/main/java/com/eugene/weather/controller/WeatherApiController.java
+++ src/main/java/com/eugene/weather/controller/WeatherApiController.java
@@ -1,6 +1,8 @@
 package com.eugene.weather.controller;
 
 import com.eugene.weather.data.SensorData;
+import com.eugene.weather.service.WeatherService;
+import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +11,9 @@ import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/v1")
+@AllArgsConstructor
 public class WeatherApiController {
+    private final WeatherService weatherService;
 
     @GetMapping(path = "/{sensorId}/data/avg",
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -18,13 +22,13 @@ public class WeatherApiController {
                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                                 @RequestParam("to")
                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-        return new SensorData(sensorId,startDate);
+        return weatherService.getSensorData(sensorId,startDate, endDate);
     }
 
-    @PostMapping(path = "/update/",
+    @PostMapping(path = "/add/",
                 consumes = MediaType.APPLICATION_JSON_VALUE,
                 produces = MediaType.APPLICATION_JSON_VALUE)
     public SensorData postSensorData(@RequestBody SensorData SensorData) {
-        return SensorData;
+        return weatherService.addSensorData(SensorData);
     }
 }
---
+++ src/main/java/com/eugene/weather/service/WeatherService.java
@@ -0,0 +1,16 @@
+package com.eugene.weather.service;
+
+import com.eugene.weather.data.SensorData;
+
+import java.time.LocalDate;
+
+public class WeatherService {
+
+    public SensorData getSensorData(String sensorId, LocalDate startDate, LocalDate endDate) {
+        return null;
+    }
+
+    public SensorData addSensorData(SensorData sensorData) {
+        return null;
+    }
+}
