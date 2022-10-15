8dbc4478910b27e73d64bb5d0a87540c4f01ca7f
Evg <redin.job@gmail.com>
2022-10-15 21:03:42 +0300

    add integration tests task

---
+++ src/integrationTest/java/com/eugene/weather/GetWeatherDataIT.java
@@ -0,0 +1,13 @@
+package com.eugene.weather;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class GetWeatherDataIT {
+
+    @Test
+    void test(){
+        Assertions.assertEquals("a", "a");
+    }
+}
