874773ef61091755019e467716876272f3f7f0d1
Evg <redin.job@gmail.com>
2022-10-16 12:59:40 +0300

    add integration tests

--- src/integrationTest/java/com/eugene/weather/BaseSpringIT.java
+++ src/integrationTest/java/com/eugene/weather/BaseSpringIT.java
@@ -1,9 +1,24 @@
 package com.eugene.weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
-@Profile("test")
+@ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 public class BaseSpringIT {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected MongoTemplate mongoTemplate;
+
 }
--- src/integrationTest/java/com/eugene/weather/GetWeatherDataIT.java
+++ src/integrationTest/java/com/eugene/weather/GetWeatherDataIT.java
@@ -1,13 +1,37 @@
 package com.eugene.weather;
 
-import org.junit.jupiter.api.Assertions;
+import com.eugene.weather.repository.SensorDTO;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 
-public class GetWeatherDataIT extends BaseSpringIT{
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class GetWeatherDataIT extends BaseSpringIT {
 
     @Test
-    void test(){
-        Assertions.assertEquals("a", "a");
+    void returns404WhenSensorIsNotFound() throws Exception {
+        mockMvc.perform(get("/v1/sensor/NON_EXISTENT_SENSOR/data/avg"))
+                .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    void returnsSensorForCurrentDate() throws Exception {
+        LocalDate currentDate = LocalDate.now();
+        SensorDTO sensorDTO = new SensorDTO("Dublin-1", currentDate, 15);
+
+        mongoTemplate.insert(sensorDTO);
+
+
+        mockMvc.perform(get("/v1/sensor/Dublin-1/data/avg"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.date").value(currentDate.toString()))
+                .andExpect(jsonPath("$.sensorId").value("Dublin-1"))
+                .andExpect(jsonPath("$.temperature").value("15"))
+                .andReturn();
     }
 }
