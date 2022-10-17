c8674f1d971e423fb2bd9e9797bfc6429653469c
Evg <redin.job@gmail.com>
2022-10-17 14:19:48 +0300

    Make Data field non null

--- src/main/java/com/eugene/weather/controller/data/DatedSensorMetrics.java
+++ src/main/java/com/eugene/weather/controller/data/DatedSensorMetrics.java
@@ -1,10 +1,13 @@
 package com.eugene.weather.controller.data;
 
+import lombok.NonNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-public record DatedSensorMetrics(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-                                 LocalDate date,
-                                 int temperature) {
+public record DatedSensorMetrics(
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        @NonNull
+        LocalDate date,
+        int temperature) {
 }
