dd06fc378584090b658aa756e0c9f340d416063a
Evg <redin.job@gmail.com>
2022-10-23 23:52:11 +0300

    fix tests

---
+++ src/test/java/com/eugene/weather/BaseTest.java
@@ -0,0 +1,68 @@
+package com.eugene.weather;
+
+import com.eugene.weather.repository.SensorRepository;
+import com.eugene.weather.repository.data.AverageData;
+import com.eugene.weather.repository.data.SensorData;
+import com.eugene.weather.repository.data.SensorDayData;
+import com.eugene.weather.service.WeatherAggregationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public abstract class BaseTest {
+    protected static final LocalDate DATE = LocalDate.of(2007, 1, 1);
+    protected static final LocalDate NEXT_DATE = DATE.plusDays(1);
+
+    SensorRepository repositoryMock = mock(SensorRepository.class);
+    ArgumentCaptor<SensorData> argumentCaptor = ArgumentCaptor.forClass(SensorData.class);
+    WeatherAggregationService sut;
+
+    @BeforeEach
+    protected void setUp() {
+        sut = new WeatherAggregationService(repositoryMock);
+    }
+
+
+    protected SensorData captureAddRepositoryCall() {
+        verify(repositoryMock).addSensorData(argumentCaptor.capture());
+        return argumentCaptor.getValue();
+    }
+
+
+    protected SensorDayData getAverageTemperature(SensorData result, LocalDate date) {
+        return result.datedSensorParams().get(date.toString());
+    }
+
+    protected void assertParamsEquals(double avg, double sum, int count, SensorDayData sensorDayData) {
+        AverageData temperature = sensorDayData.temperature();
+        assertEquals(avg, temperature.avg());
+        assertEquals(sum, temperature.sum());
+        assertEquals(count, temperature.count());
+    }
+
+
+    protected void mockRepositoryGetSensorData(String sensorId, Map<String, SensorDayData> map) {
+        SensorData sensorData = new SensorData(sensorId, map);
+        Mockito.when(repositoryMock.getSensorData(eq(sensorId))).thenReturn(sensorData);
+    }
+
+
+    protected SensorData captureUpdateRepositoryCall() {
+        verify(repositoryMock).updateSensorData(argumentCaptor.capture());
+        return argumentCaptor.getValue();
+    }
+
+    protected SensorDayData createSensorDayData(double sum, int count) {
+        double avg = sum / count;
+        return new SensorDayData(new AverageData(avg, sum, count),
+                new AverageData(avg, sum, count));
+    }
+}
---
+++ src/test/java/com/eugene/weather/GetWeatherAggregationServiceTest.java
@@ -0,0 +1,152 @@
+package com.eugene.weather;
+
+import com.eugene.weather.controller.data.FramedSensorMetrics;
+import com.eugene.weather.repository.data.SensorData;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GetWeatherAggregationServiceTest extends BaseTest {
+
+
+    @Test
+    void testGetsAverageBetweenDates() {
+        String id = "testId";
+        String firstDate = "2007-01-01";
+        String secondDate = "2007-01-02";
+        LocalDate startDate = LocalDate.parse(firstDate).minusDays(1);
+        LocalDate endDate = LocalDate.parse(secondDate).plusDays(1);
+        mockRepositoryGetSensorData(id,
+                Map.of(firstDate, createSensorDayData(10.0, 1),
+                        secondDate, createSensorDayData(20.0, 1)));
+
+
+        FramedSensorMetrics result = sut.getSensorData(id, startDate, endDate);
+
+        assertEquals("testId", result.sensorId());
+        assertEquals(15.0, result.metrics().temperature());
+    }
+
+    @Test
+    void testGetReturnsNanWhenStartDateIsBiggerThatEndDate() {
+        String id = "testId";
+        LocalDate startDate = LocalDate.parse("2007-01-01");
+        LocalDate endDate = startDate.minusDays(1);
+        mockRepositoryGetSensorData(id,
+                Map.of(startDate.toString(), createSensorDayData(10.0, 1),
+                        endDate.toString(), createSensorDayData(20.0, 1)));
+
+
+        FramedSensorMetrics result = sut.getSensorData(id, startDate, endDate);
+
+        assertEquals(Double.NaN, result.metrics().temperature());
+    }
+
+    @Test
+    void testGetReturnsNanWhenThereIsNoData() {
+        String id = "testId";
+        LocalDate startDate = LocalDate.parse("2007-01-01");
+        LocalDate endDate = LocalDate.parse("2007-01-02");
+        mockRepositoryGetSensorData(id, Map.of());
+
+
+        FramedSensorMetrics result = sut.getSensorData(id, startDate, endDate);
+
+        assertEquals(Double.NaN, result.metrics().temperature());
+    }
+
+    @Test
+    void testGetDoesNotIncludeDatesOutLimit() {
+        String id = "testId";
+        String firstDate = "2007-01-01";
+        String secondDate = "2007-01-02";
+        String thirdDate = "2007-01-03";
+        LocalDate startDate = LocalDate.parse(firstDate).minusDays(1);
+        LocalDate endDate = LocalDate.parse(secondDate).plusDays(1);
+        mockRepositoryGetSensorData(id,
+                Map.of(firstDate, createSensorDayData(10, 1),
+                        secondDate, createSensorDayData(20, 1),
+                        thirdDate, createSensorDayData(30, 1)));
+
+
+        FramedSensorMetrics result = sut.getSensorData(id, startDate, endDate);
+
+        assertEquals(15.0, result.metrics().temperature());
+        assertEquals(startDate, result.startDate());
+        assertEquals(endDate, result.endDate());
+    }
+
+    @Test
+    void testGetAllDataDoesNotIncludeDatesOutLimit() {
+        String firstDate = "2007-01-01";
+        String secondDate = "2007-01-02";
+        String thirdDate = "2007-01-03";
+
+        Mockito.when(repositoryMock.getAllSensorsData())
+                .thenReturn(List.of(
+                        new SensorData("firstSensor", Map.of(
+                                firstDate, createSensorDayData(5, 1),
+                                secondDate, createSensorDayData(15, 1),
+                                thirdDate, createSensorDayData(30, 1)
+                        )),
+                        new SensorData("secondSensor", Map.of(
+                                firstDate, createSensorDayData(20, 1),
+                                secondDate, createSensorDayData(30, 1),
+                                thirdDate, createSensorDayData(40, 1)
+                        ))));
+
+        LocalDate startDate = LocalDate.parse(secondDate).minusDays(1);
+        LocalDate endDate = LocalDate.parse(thirdDate).plusDays(1);
+
+        FramedSensorMetrics result = sut.getAllSensorsData(startDate, endDate);
+
+        assertEquals(28.75, result.metrics().temperature());
+        assertEquals(startDate, result.startDate());
+        assertEquals(endDate, result.endDate());
+    }
+
+    @Test
+    void testAllDataGetsAverageBetweenDates() {
+        String firstDate = "2007-01-01";
+        String secondDate = "2007-01-02";
+
+        Mockito.when(repositoryMock.getAllSensorsData())
+                .thenReturn(List.of(
+                        new SensorData("firstSensor", Map.of(
+                                firstDate, createSensorDayData(5, 1),
+                                secondDate, createSensorDayData(15, 1)
+                        )),
+                        new SensorData("secondSensor", Map.of(
+                                firstDate, createSensorDayData(20, 1),
+                                secondDate, createSensorDayData(30, 1)
+                        ))));
+
+        LocalDate startDate = LocalDate.parse(firstDate).minusDays(1);
+        LocalDate endDate = LocalDate.parse(secondDate).plusDays(1);
+
+        FramedSensorMetrics result = sut.getAllSensorsData(startDate, endDate);
+
+        assertEquals("all", result.sensorId());
+        assertEquals(17.5, result.metrics().temperature());
+    }
+
+    @Test
+    void testGetAllReturnsNanWhenStartDateIsBiggerThatEndDate() {
+        LocalDate startDate = LocalDate.parse("2007-01-01");
+        LocalDate endDate = startDate.minusDays(1);
+        Mockito.when(repositoryMock.getAllSensorsData())
+                .thenReturn(List.of(
+                        new SensorData("firstSensor", Map.of(
+                                startDate.toString(), createSensorDayData(5, 1)))));
+
+
+        FramedSensorMetrics result = sut.getAllSensorsData(startDate, endDate);
+
+        assertEquals(Double.NaN, result.metrics().temperature());
+    }
+}
